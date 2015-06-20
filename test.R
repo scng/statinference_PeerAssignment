59d95e0aab8780bdf7d11611cd2993613516088b
xlogger <xloggerster@gmail.com>
2015-06-21 02:44:58 +0800

    part 2 first commit

--- test.R
+++ test.R
@@ -17,5 +17,29 @@ require(ggplot2)
 sample_df <- as.data.frame(exp_sample_means)
 g <- ggplot(sample_df, aes(x=exp_sample_means))
 g <- g + geom_histogram(aes(y = ..density..), binwidth=0.35, fill='blue', color='white')
-g <- g + stat_function(fun = dnorm, args=list(mean=5, sd=5/sqrt(sample_size)), bin_width=0.5, color='red')
+g <- g + stat_function(fun = dnorm, args=list(mean=1/lambda, sd=(1/lambda)/sqrt(sample_size)), bin_width=0.5, color='red')
+g <- g + ggtitle("Comparison between Distribution of Simulation and Normal Distribution")
+g <- g + xlab("mean") + ylab("density")
+#g <- g + geom_vline(xintercept = mean(exp_sample_means), color='green')
+#g <- g + geom_vline(xintercept = 1/lambda, color='purple')
 g
+
+require(datasets)
+?ToothGrowth
+#str(ToothGrowth)
+summary(ToothGrowth)
+
+table(ToothGrowth$supp, ToothGrowth$dose)
+
+g2 <- ggplot(data=ToothGrowth, aes(x=as.factor(dose), y=len, fill=supp)) 
+g2 <- g2 + geom_boxplot()
+g2 <- g2 + facet_grid(. ~ supp) 
+g2 <- g2 + xlab("Dose in miligrams") + ylab("Tooth length") 
+g2 <- g2 + guides(fill = guide_legend(title ="Supplement type"))
+g2
+
+t1 <- t.test(len~supp, paired = FALSE, var.equal = TRUE, data=ToothGrowth)
+t1
+t1$statistics
+
+
