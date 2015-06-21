67e99201fbd21b1aaa7ce0578d5f0b884d198213
xlogger <xloggerster@gmail.com>
2015-06-21 15:30:03 +0800

    Refined part 1 and part 2 reports

--- test.R
+++ test.R
@@ -38,8 +38,32 @@ g2 <- g2 + xlab("Dose in miligrams") + ylab("Tooth length")
 g2 <- g2 + guides(fill = guide_legend(title ="Supplement type"))
 g2
 
-t1 <- t.test(len~supp, paired = FALSE, var.equal = TRUE, data=ToothGrowth)
+#t1 <- t.test(len~supp, paired = FALSE, var.equal = TRUE, data=ToothGrowth)
+t1 <- t.test(len~supp, paired = FALSE, data=ToothGrowth)
 t1
-t1$statistics
 
 
+ToothGrowth.05 <- ToothGrowth[ToothGrowth$dose == 0.5, "len"]
+ToothGrowth.10 <- ToothGrowth[ToothGrowth$dose == 1.0, "len"]
+ToothGrowth.20 <- ToothGrowth[ToothGrowth$dose == 2.0, "len"]
+
+t.05.10 <- t.test(ToothGrowth.05, ToothGrowth.10, paired = FALSE, var.equal = TRUE)
+
+t.10.20 <- t.test(ToothGrowth.10, ToothGrowth.20, paired = FALSE, var.equal = TRUE)
+
+t.05.20 <- t.test(ToothGrowth.05, ToothGrowth.20, paired = FALSE, var.equal = TRUE)
+
+t.05.10
+t.10.20
+t.05.20
+
+ToothGrowth.05.VC <- ToothGrowth[ToothGrowth$dose == 0.5 & ToothGrowth$supp == "VC", "len"]
+ToothGrowth.05.OJ <- ToothGrowth[ToothGrowth$dose == 0.5 & ToothGrowth$supp == "OJ", "len"]
+ToothGrowth.10.VC <- ToothGrowth[ToothGrowth$dose == 1.0 & ToothGrowth$supp == "VC", "len"]
+ToothGrowth.10.OJ <- ToothGrowth[ToothGrowth$dose == 1.0 & ToothGrowth$supp == "OJ", "len"]
+ToothGrowth.20.VC <- ToothGrowth[ToothGrowth$dose == 2.0 & ToothGrowth$supp == "VC", "len"]
+ToothGrowth.20.OJ <- ToothGrowth[ToothGrowth$dose == 2.0 & ToothGrowth$supp == "OJ", "len"]
+
+t.05 <- t.test(ToothGrowth.05.VC, ToothGrowth.05.OJ, paired = FALSE)
+t.10 <- t.test(ToothGrowth.10.VC, ToothGrowth.10.OJ, paired = FALSE)
+t.20 <- t.test(ToothGrowth.20.VC, ToothGrowth.20.OJ, paired = FALSE)
